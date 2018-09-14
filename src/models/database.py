9bdff7a446bc1aa5351ed5d9409c9d1420cc90c9
Rodolfo De Nadai <rdenadai@gmail.com>
2018-09-14 10:14:14 -0300

    - Starting db var with None.

--- src/models/database.py
+++ src/models/database.py
@@ -2,6 +2,7 @@ from peewee import *
 from ..app import application
 
 
+db = None
 if application.config.get('DATABASE_TYPE') == 'sqlite':
     db = SqliteDatabase(application.config.get('DATABASE_CONN'))
 elif application.config.get('DATABASE_TYPE') == 'psql':
