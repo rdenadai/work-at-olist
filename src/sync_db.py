bbb5bfb321d5016fdc24c5ea5810c0ad338aa4d7
Rodolfo De Nadai <rdenadai@gmail.com>
2018-09-14 17:00:00 -0300

    - Correct Bill table fields and records;  - Created new table BillHistory to record calculated Bill history;  - Correct all tests;  - Correct syncdb.

--- src/sync_db.py
+++ src/sync_db.py
@@ -1,12 +1,12 @@
 import logging
 import sys
 from .app import application, db_conn
-from .models.models import Telephone, Call, Bill
+from .models.models import Telephone, Call, Bill, BillHistory
 
 if application.config.get('DEBUG', False):
     logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 else:
     logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 logging.info('Sync database structure...')
-db_conn.create_tables([Telephone, Call, Bill])
+db_conn.create_tables([Telephone, Call, Bill, BillHistory])
 logging.info('Sync ended...')
