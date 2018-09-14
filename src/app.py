ab7d12c15e3b2db368678c35b2575df0f36938ad
Rodolfo De Nadai <rdenadai@gmail.com>
2018-09-14 10:05:56 -0300

    - Comments add to explain steps.

--- src/app.py
+++ src/app.py
@@ -5,12 +5,15 @@ from .controllers.api import api
 from .settings import DevelopmentConfig, ProductionConfig
 
 
+# Start the flask app
 app = Flask(__name__)
 
+# Import configuration depending on the environment
 if os.environ.get('PYTHONHOME', None):
     app.config.from_object(ProductionConfig)
 else:
     app.config.from_object(DevelopmentConfig)
 
+# Register the apps blueprint endpoints
 app.register_blueprint(principal)
 app.register_blueprint(api)
